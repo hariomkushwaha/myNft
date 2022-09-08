02165fbba215109da38be507726d66a2f321036a
hariomkushwaha <hariomkushwaha1947@gmail.com>
2022-09-08 14:59:53 +0530

    writefile

--- jsontests.js
+++ jsontests.js
@@ -1,17 +1,39 @@
-const output = [];
-let input = ["John","Hari","James"]
-let tmp;
+const json = require('big-json');
+const fs = require('fs')
+async function main(){
 
-for(let i = 0; i < 5000000; i++){
-  tmp = {"name" : input[i%3],"id": 0,
-  "work": "Unilogic",
-  "email": "adam.carter@unilogic.com",
-  "dob": "1978",
-  "address": "83 Warner Street",
-  "city": "Boston"
-  };
-  output.push(tmp);
+const output = []
+for (let i = 0; i < 1000000; i++) {
+  output.push({
+    name: "John",
+    id: 0,
+    work: "Unilogic",
+    email: "adam.carter@unilogic.com",
+    dob: "1978",
+    address: "83 Warner Street",
+    city: "Boston",
+  });
+  output.push({
+    name: "Hari",
+    id: 0,
+    work: "Unilogic",
+    email: "adam.carter@unilogic.com",
+    dob: "1978",
+    address: "83 Warner Street",
+    city: "Boston",
+  });
+  output.push({
+    name: "James",
+    id: 0,
+    work: "Unilogic",
+    email: "adam.carter@unilogic.com",
+    dob: "1978",
+    address: "83 Warner Street",
+    city: "Boston",
+  });
 }
+
+console.log("json gen..");
 
 // console.log(JSON.stringify(output));
 // let out="[";
@@ -27,7 +49,7 @@ for(let i = 0; i < 5000000; i++){
 
 
 
-const json = require('big-json');
+
 
 
 // // setTimeout(() => {
@@ -44,19 +66,48 @@ async function asyncCall(output) {
   });
  
 await stringifyStream.on('data', function(strChunk) {
-   dataJ+=strChunk;
-   dataJs.push(dataJ);
-    // console.log(strChunk);
-    // console.log(dataJ);
+  
+  let s = dataJ+=strChunk;
+  s+='\n'
+fs.appendFile('output.txt',s, (err) => {
+  if (err) throw err;
 });
-setTimeout(() => {
-  console.log(dataJs[dataJs.length-1]);
+// console.log(strChunk);
 
-console.log("done")
-  //Runs one at a time, need to use a callback for that part to work
-}, 120000);
+});
+
+// setTimeout(() => {
+//   fs.writeFile('output.txt', dataJ, (err) => {
+
+//     if (err) throw err;
+// })
+// console.log("file written...")
+//   //Runs one at a time, need to use a callback for that part to work
+// }, 6000);
+
+
 
 }
 asyncCall(output);
 
+// uncomment from 94 to 97 and run file again to append the str a last
+
+// let str ="this is end"
+//   fs.appendFile('Output.txt',str, (err) => {
+//     if (err) throw err;
+//   });
+
+
+
+}
+
+main()
 // console.log(dataJs);
+
+
+  //  dataJs.push(dataJ);
+//    fs.writeFile('Output.txt', strChunk, (err) => {
+
+//     if (err) throw err;
+//     // console.log("file written...")
+// })
